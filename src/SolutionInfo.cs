f7b3e6ae1de7df4982cb0d10d62c3d18adab96d9
markkemper1 <markkemper1@gmail.com>
2012-03-11 23:22:03 +0000

    Updating nuspec description.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.9")]
-[assembly: AssemblyFileVersion("1.0.0.9")]
+[assembly: AssemblyVersion("1.0.0.10")]
+[assembly: AssemblyFileVersion("1.0.0.10")]
 
 
