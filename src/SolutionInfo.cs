ba96235e0f878fc9dc31a612b46ebf8fdc16aba7
unknown <markkemper1@gmail.com>
2014-01-19 14:19:57 +1100

    v1.1.2

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.1.39")]
-[assembly: AssemblyFileVersion("1.1.1.39")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 
 
